20777315bed1903bf1b47bd60c8fcef961a81451
Rajendra kadam <rajendrakadam249@gmail.com>
2015-12-22 12:23:27 +0000

    Initial Commit

---
+++ client/helpers.js
@@ -0,0 +1,37 @@
+/*global Chats*/
+
+Template.chat_page.helpers({
+    
+    messages:function(){
+      var chat = Chats.findOne({_id:Session.get("chatId")});
+      return chat.messages;
+    }, 
+    
+    other_user:function(){
+      return "";
+    } 
+
+});
+
+Template.available_user_list.helpers({
+    users:function(){
+      return Meteor.users.find();
+    }
+});
+
+
+Template.available_user.helpers({
+    getUsername:function(userId){
+      var user = Meteor.users.findOne({_id:userId});
+      return user.profile.username;
+    }, 
+    
+    isMyUser:function(userId){
+      if (userId == Meteor.userId()){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+});
